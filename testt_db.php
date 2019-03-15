cb4690905eabe8ffcbf95dc90be0e50691dc2bd3
DanEC1994 <klase_69@hotmail.com>
2019-03-15 12:26:09 -0600

    Base de datos agregada y boton registrar funcionando

--- testt_db.php
+++ testt_db.php
@@ -1,6 +1,19 @@
 <?php
-
 include("class_registro_dal.php");
+
+$matricula=$_POST["imatricula"];
+$nombre=$_POST["inombre"];
+$correo=$_POST["icorreo"];
+$telefono=$_POST["itelefono"];
+$grado=$_POST["sgrado"];
+$plan=$_POST["splan"];
+$carrera=$_POST["scarrera"];
+$materias=$_POST["smaterias"];
+$estatus=$_POST["status"];
+
+echo "Los datos $matricula , $nombre , $correo , $telefono , $grado , $plan , $carrera , $materias , $estatus se registraron!";
+
+
 //$obj = new class_db();
 $obj2=new registro_dal();
 $resultado2=$obj2->  get_datos_by_matricula(13214207);
